663701d1c6d80b67802d6c300a607e1cbddcfc3e
Korport <63680787+Korport@users.noreply.github.com>
2023-10-05 15:48:01 -0700

    Create Logger.ps1

---
+++ Logger.ps1
@@ -0,0 +1,25 @@
+Function Log-Output
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Output $(Get-Date)]$message"
+}
+Function Log-Info
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Info $(Get-Date)]$message"
+}
+Function Log-Warning
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Warning $(Get-Date)]$message"
+}
+Function Log-Error
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Error $(Get-Date)]$message"
+}
+Function Log-Debug
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Debug $(Get-Date)]$message"
+}
